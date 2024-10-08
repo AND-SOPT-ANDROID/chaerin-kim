801f38b6588e1f471a313ff67ea3cbb50d1f134a
chrin05 <rinring105@gmail.com>
2024-10-08 18:59:19 +0900

    feat : MainScreen 추가 및 SignUpActivity에 연결

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -1,15 +1,18 @@
 package org.sopt.and
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ANDANDROIDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ANDANDROIDTheme {
-        Greeting("Android")
-    }
+fun MainScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val intent = Intent(context, SignUpActivity::class.java)
+    Button(
+        onClick = {
+            context.startActivity(intent)
+        }
+    ) { }
 }
