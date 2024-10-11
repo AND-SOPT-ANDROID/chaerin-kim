af002b015b806013dfb3205da922cd298ccaf6a9
chrin05 <rinring105@gmail.com>
2024-10-11 12:38:32 +0900

    feat : 권장사항 메시지 component 사이즈 조절

--- app/src/main/java/org/sopt/and/component/NotificationMessage.kt
+++ app/src/main/java/org/sopt/and/component/NotificationMessage.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.sopt.and.R
 import org.sopt.and.ui.theme.Gray40
 import org.sopt.and.ui.theme.pretendardFamily
@@ -22,13 +23,14 @@ fun TextFieldNotificationMessage(message: String) {
             painter = painterResource(R.drawable.ic_alert_circle),
             contentDescription = "btn_close",
             modifier = Modifier
-                .size(24.dp)
+                .size(22.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
             text = message,
             color = Gray40,
-            fontFamily = pretendardFamily
+            fontFamily = pretendardFamily,
+            fontSize = 14.sp
         )
     }
 }
@@ -36,5 +38,5 @@ fun TextFieldNotificationMessage(message: String) {
 @Preview
 @Composable
 fun Preview(modifier: Modifier = Modifier) {
-    TextFieldNotificationMessage("로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을 입력해주세요.")
+    TextFieldNotificationMessage("로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을 입력해 주세요.")
 }
