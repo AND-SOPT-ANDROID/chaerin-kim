3be39e2315e00780ac05605b7e4adf26058fa11e
chrin05 <rinring105@gmail.com>
2024-10-11 15:53:46 +0900

    feat : myActivity 추가

---
+++ app/src/main/java/org/sopt/and/MyActivity.kt
@@ -0,0 +1,33 @@
+package org.sopt.and
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.and.screen.MyScreen
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+
+class MyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val userName = "채린"
+                    MyScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        userName
+                    )
+                }
+            }
+        }
+    }
+}
