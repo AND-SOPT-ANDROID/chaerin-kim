ec006808c5cb47f0ff37de9e795c22faebc1c0bf
chrin05 <rinring105@gmail.com>
2024-10-11 19:27:18 +0900

    feat : 비밀번호 show 버튼 구현

--- app/src/main/java/org/sopt/and/component/CustomTextField.kt
+++ app/src/main/java/org/sopt/and/component/CustomTextField.kt
@@ -1,7 +1,12 @@
 package org.sopt.and.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -9,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,13 +35,18 @@ fun GrayTextField(
     onValueChange: (String) -> Unit
 ) {
     var text by remember { mutableStateOf(value) }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
 
     val visualTransformation = if (placeholderText.contains("비밀번호")) {
-        PasswordVisualTransformation()
+        if (!passwordHidden) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        }
     } else {
         VisualTransformation.None
     }
-    
+    val trailingText = if (placeholderText.contains("비밀번호")) { "show" } else { "" }
     TextField(
         value = text,
         onValueChange = {
@@ -63,5 +74,16 @@ fun GrayTextField(
             cursorColor = Color.White
         ),
         modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            Text(
+                text = trailingText,
+                modifier = Modifier
+                    .clickable { passwordHidden = !passwordHidden }
+                    .padding(end = 8.dp),
+                color = Color.White,
+                fontFamily = pretendardFamily,
+                fontWeight = FontWeight.Medium,
+            )
+        }
     )
 }
