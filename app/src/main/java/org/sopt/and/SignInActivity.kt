c306353331b63b03dc3fd38614875b2eb5e1e98f
chrin05 <rinring105@gmail.com>
2024-10-11 14:32:29 +0900

    feat : SignIn 화면 생성

---
+++ app/src/main/java/org/sopt/and/SignInActivity.kt
@@ -0,0 +1,29 @@
+package org.sopt.and
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.and.screen.SignInScreen
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+
+class SignInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SignInScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/sopt/and/screen/SignInScreen.kt
@@ -0,0 +1,12 @@
+package org.sopt.and.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignInScreen(modifier: Modifier = Modifier) {
+    Column {
+
+    }
+}
