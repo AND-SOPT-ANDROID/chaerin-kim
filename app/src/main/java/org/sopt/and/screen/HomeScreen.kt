3f6b3b4ce0a504efc1d35168b2196668ea77b130
chrin05 <rinring105@gmail.com>
2024-10-25 20:37:18 +0900

    feat: 상하 스크롤 구현

--- app/src/main/java/org/sopt/and/screen/HomeScreen.kt
+++ app/src/main/java/org/sopt/and/screen/HomeScreen.kt
@@ -1,10 +1,13 @@
 package org.sopt.and.screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,23 +20,40 @@ import org.sopt.and.component.HomeTopBar
 import org.sopt.and.component.TodayTop20
 import org.sopt.and.data.HomeBannerItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column(
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
-            .padding(horizontal = 14.dp)
+            .padding(horizontal = 14.dp),
+        contentPadding = PaddingValues(vertical = 12.dp)
     ) {
-        HomeTopBar()
-        Spacer(modifier = Modifier.height(10.dp))
-        HomeCategory()
-        Spacer(modifier = Modifier.height(16.dp))
-        HomeBanner()
-        EditorRecommended()
+        item {
+            HomeTopBar()
+            Spacer(modifier = Modifier.height(6.dp))
+        }
 
-        TodayTop20()
+        stickyHeader {
+            HomeCategory()
+            Spacer(modifier = Modifier.height(6.dp))
+        }
+
+        item {
+            HomeBanner()
+            Spacer(modifier = Modifier.height(6.dp))
+        }
+
+        item {
+            EditorRecommended()
+            Spacer(modifier = Modifier.height(6.dp))
+        }
+
+        item {
+            TodayTop20()
+            Spacer(modifier = Modifier.height(10.dp))
+        }
     }
-
 }
 
 @Preview
