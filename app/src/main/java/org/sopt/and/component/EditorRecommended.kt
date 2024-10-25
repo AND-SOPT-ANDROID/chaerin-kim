1bfc6a76c7e8d7d6330d23d4f3baee9cae2fbf86
chrin05 <rinring105@gmail.com>
2024-10-25 19:56:31 +0900

    feat: 에디터추천 LazyRow 연결

--- app/src/main/java/org/sopt/and/component/EditorRecommended.kt
+++ app/src/main/java/org/sopt/and/component/EditorRecommended.kt
@@ -1,15 +1,19 @@
 package org.sopt.and.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun EditorRecommended() {
-    Column {
+    Column(
+        modifier = Modifier.padding(top = 14.dp)
+    ) {
         ContentListTitle("믿고 보는 웨이브 에디터 추천작") { }
-
+        ContentLazyRow()
     }
 }
 
