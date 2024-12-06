bc62bf0d8b4789f828fe6e157f917267fc494489
chrin05 <rinring105@gmail.com>
2024-12-06 22:17:43 +0900

    refactor/#8: userRepository 수정

--- app/src/main/java/org/sopt/and/domain/repository/UserRepository.kt
+++ app/src/main/java/org/sopt/and/domain/repository/UserRepository.kt
@@ -1,27 +1,12 @@
 package org.sopt.and.domain.repository
 
-import org.sopt.and.data.dto.request.RequestSignIn
-import org.sopt.and.data.dto.request.RequestSignUp
-import org.sopt.and.data.dto.response.ResponseMyHobby
-import org.sopt.and.data.dto.response.ResponseSignIn
-import org.sopt.and.data.dto.response.ResponseSignUp
-import org.sopt.and.data.remote.ServicePool
+import org.sopt.and.domain.entity.ResponseMyHobby
+import org.sopt.and.domain.entity.ResponseSignIn
+import org.sopt.and.domain.entity.ResponseSignUp
+import org.sopt.and.domain.entity.UserInfo
 
-class UserRepository {
-    private val service = ServicePool.userService
-
-    suspend fun signUpUser(request: RequestSignUp): Result<ResponseSignUp> = runCatching {
-        val response = service.signUpUser(request)
-        response.result ?: throw Throwable("Failed to SignUp")
-    }
-
-    suspend fun signInUser(request: RequestSignIn) : Result<ResponseSignIn> = runCatching {
-        val response = service.userLogin(request)
-        response.result ?: throw Throwable("Failed to SignIn")
-    }
-
-    suspend fun getMyHobby(request: String) : Result<ResponseMyHobby> = runCatching {
-        val response = service.getMyHobby(request)
-        response.result ?: throw Throwable("Failed to GetMyHobby")
-    }
+interface UserRepository {
+    suspend fun signUpUser(request: UserInfo) : Result<ResponseSignUp>
+    suspend fun signInUser(request: UserInfo) : Result<ResponseSignIn>
+    suspend fun getMyHobby(request: String) : Result<ResponseMyHobby>
 }
